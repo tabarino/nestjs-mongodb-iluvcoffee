93ae32f245a34bb97affab87a9ba13690cdd7f3c
tabarino <tabarino@outlook.com>
2020-12-27 11:28:03 +0000

    Creating a Mongoose Model

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,8 +1,15 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { CoffeesController } from './controllers/coffees.controller';
+import { Coffee, CoffeeSchema } from './entities/coffee.entity';
 import { CoffeesService } from './services/coffees.service';
 
 @Module({
+    imports: [
+        MongooseModule.forFeature([
+            { name: Coffee.name, schema: CoffeeSchema }
+        ])
+    ],
     controllers: [
         CoffeesController
     ],
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,20 @@
-export class Coffee {
-    id: number;
+import { Prop, Schema, SchemaFactory } from "@nestjs/mongoose";
+import { Document } from "mongoose";
+
+// Mongo automatically makes all collections plural and lowercase, in this case (coffees)
+@Schema()
+export class Coffee extends Document {
+    // Mongo automatically adds the id, so we need to remove id from here as best practice
+    // id: number;
+    
+    @Prop()
     name: string;
+
+    @Prop()
     brand: string;
+
+    @Prop([String])
     flavours: string[];
 }
+
+export const CoffeeSchema = SchemaFactory.createForClass(Coffee);
