4c704649c52aa3cb64b30695d780f5571fe2d67c
tabarino <tabarino@outlook.com>
2020-12-27 10:41:30 +0000

    Introducing the Mongoose Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CoffeesModule } from './coffees/coffees.module';
 
 @Module({
     imports: [
-        CoffeesModule
+        CoffeesModule,
+        MongooseModule.forRoot('mongodb://localhost:27017/nest-course')
     ],
     controllers: [
         AppController
